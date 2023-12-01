da8f17910de5651df751ddb56ccedbd52f275e81
Kharybdys <kharybdys@gmail.com>
2023-12-01 18:01:40 +0100

    Added solution for puzzle1a

--- puzzle1a/solver.py
+++ puzzle1a/solver.py
@@ -1,3 +1,17 @@
+import re
+
+
+PATTERN = re.compile(r"\D*")
+
+
+def parse_line(line: str) -> int:
+    digits = list(filter(bool, PATTERN.split(line)))
+    print(f"{line=}, {digits=}")
+    result = int(f"{digits[0]}{digits[-1]}")
+    print(f"{result=}")
+    return result
+
 
 def solve(puzzle_input: list[str]) -> None:
-    print(puzzle_input)
+    solution = sum(map(parse_line, puzzle_input))
+    print(f"Solution is {solution}")
