6de5c6492a452536c27c161c282f72652d46f9b4
Kharybdys <kharybdys@gmail.com>
2023-12-03 15:46:32 +0100

    Improved runtime of algorithm for 2021, day 24, part a. Runtime is now in the solution_step instead of merge_solutions part as it should be. Increased sample size to -1M/+1M as -10000/+10000 provided no solution

--- puzzle2021_24/solver.py
+++ puzzle2021_24/solver.py
@@ -12,8 +12,11 @@ class PartialSolution:
     partial_id: str
     required_z: int
 
-    def new_solution(self, w: int, z: int) -> Self:
+    def new_solution(self, w: str, z: int) -> Self:
         return PartialSolution(partial_id=f"{w}{self.partial_id}", required_z=z)
+
+    def __str__(self) -> str:
+        return f"PartialSolution[{self.partial_id=}, {self.required_z=}]"
 
 
 def split_to_instruction_sets(puzzle_input: list[str]) -> Generator[list[str], None, None]:
@@ -33,32 +36,58 @@ def split_to_instruction_sets(puzzle_input: list[str]) -> Generator[list[str], N
 
 
 def generate_partial_alus() -> Generator[PartialALU, None, None]:
-    MIN_Z = -10000
-    MAX_Z = +10000
+    MIN_Z = -1000000
+    MAX_Z = +1000000
     for w in range(1, 10):
         for z in range(MIN_Z, MAX_Z+1):
             yield PartialALU(w, z=z)
 
 
-def solution_step(partial_solutions: list[PartialSolution], instruction_set: list[str]) -> list[PartialSolution]:
-    new_solutions: list[PartialSolution] = []
+def solution_step(instruction_set: list[str]) -> dict[int, PartialSolution]:
+    new_solutions: dict[int, PartialSolution] = {}
     if analyze_instructions(instruction_set) != (True, False, False, True):
         raise NotImplementedError("This solver cannot handle inputs from previous instruction sets other than w (through inp) and z")
     for partial_alu in generate_partial_alus():
         partial_alu.execute_all(instruction_set)
-        for partial_solution in filter(lambda s: s.required_z == partial_alu.z, partial_solutions):
-            new_solutions.append(partial_solution.new_solution(w=partial_alu.initial_w, z=partial_alu.initial_z))
+        earlier_solution = new_solutions.get(partial_alu.z, "")
+        if not earlier_solution or earlier_solution.partial_id < str(partial_alu.initial_w):
+            new_solutions[partial_alu.z] = PartialSolution(partial_id=str(partial_alu.initial_w),
+                                                           required_z=partial_alu.initial_z
+                                                           )
     return new_solutions
+
+
+def merge_solutions(old_solutions: dict[int, PartialSolution], new_solutions: dict[int, PartialSolution]) -> dict[int, PartialSolution]:
+    print(f"{old_solutions=}")
+    result: dict[int, PartialSolution] = {}
+    for old_z, old_solution in old_solutions.items():
+        if old_z in new_solutions:
+            partial_solution = new_solutions[old_z]
+            print(f"Looking at {old_z=}, {partial_solution=}")
+            new_solution = old_solutions[old_z].new_solution(w=partial_solution.partial_id, z=partial_solution.required_z)
+            print(f"{new_solution=}")
+            if partial_solution.required_z not in result:
+                result[partial_solution.required_z] = new_solution
+                print(f"New entry to result")
+            elif result[partial_solution.required_z].partial_id < new_solution.partial_id:
+                print(f"Replacing entry {result[partial_solution.required_z]}")
+                result[partial_solution.required_z] = new_solution
+
+    return result
 
 
 def solve_a(puzzle_input: list[str]) -> None:
     print(puzzle_input)
     t0 = time.time()
-    partial_solutions = [PartialSolution("", 0)]
+    partial_solutions = {0: PartialSolution("", 0)}
     for index, instruction_set in enumerate(reversed(list(split_to_instruction_sets(puzzle_input)))):
-        partial_solutions = solution_step(partial_solutions, instruction_set)
-        print(f"Processed instruction_set {index+1}, got {len(partial_solutions)} solutions, time: {time.time() - t0}")
-    solution = max(partial_solutions, key=lambda s: s.partial_id)
+        new_solutions = solution_step(instruction_set)
+        print(f"Processed instruction_set {index + 1}, got {len(new_solutions)} new solutions, time: {time.time() - t0}")
+        partial_solutions = merge_solutions(partial_solutions, new_solutions)
+        print(f"Processed instruction_set {index + 1}, got {len(partial_solutions)} merged solutions, time: {time.time() - t0}")
+        if not partial_solutions:
+            raise ValueError("Increase MIN_Z/MAX_Z, no solutions to be found like this!")
+    solution = max(partial_solutions.values(), key=lambda s: s.partial_id)
     print(f"Solution: {solution.partial_id}")
 
 
