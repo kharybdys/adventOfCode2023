50f52e1e4347ceaec0e81fca58cadce0090a45db
Kharybdys <kharybdys@gmail.com>
2025-12-06 17:04:42 +0100

    Remove the defaults, work from CLI from here on out (UV makes that easier)

--- advent/main.py
+++ advent/main.py
@@ -3,17 +3,11 @@ from pathlib import Path
 
 from advent.registry import get_solver, import_all_solvers
 
-# I intend to manually change this for every puzzle
-DEFAULT_YEAR = "2025"
-DEFAULT_PUZZLE = "1"
-DEFAULT_VARIATION = "a"
-EXAMPLE = False
 
-
-def load_input(year: str, key: str) -> list[str]:
+def load_input(year: str, key: str, example: bool) -> list[str]:
     data_dir = Path(__file__).parent.parent / "data"
 
-    example_postfix = "_example" if EXAMPLE else ""
+    example_postfix = "_example" if example else ""
     with open(data_dir / year / f"input{key}{example_postfix}.txt", "rt") as file:
         return [line.rstrip() for line in file]
 
@@ -22,14 +16,15 @@ def main():
     import_all_solvers()
 
     parser = ArgumentParser(prog="AdventOfCode_solver")
-    parser.add_argument("-y", "--year", default=DEFAULT_YEAR)
-    parser.add_argument("-p", "--puzzle", default=DEFAULT_PUZZLE)
-    parser.add_argument("-v", "--variation", default=DEFAULT_VARIATION)
+    parser.add_argument("-y", "--year", required=True)
+    parser.add_argument("-p", "--puzzle", required=True)
+    parser.add_argument("-v", "--variation", required=True)
+    parser.add_argument("-e", "--example", action="store_true")
     args = parser.parse_args()
     solver = get_solver(year=args.year, key=args.puzzle, variation=args.variation)
     if solver:
-        puzzle_input = load_input(year=args.year, key=args.puzzle)
-        solver(puzzle_input=puzzle_input, example=EXAMPLE)
+        puzzle_input = load_input(year=args.year, key=args.puzzle, example=args.example)
+        solver(puzzle_input=puzzle_input, example=args.example)
     else:
         raise NotImplementedError(f"No support for combination {args.year}, {args.puzzle}, {args.variation}")
 
