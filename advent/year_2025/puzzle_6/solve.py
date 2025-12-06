f941e5ced464019a9bff9536192acb932ea04b44
Kharybdys <kharybdys@gmail.com>
2025-12-06 20:20:29 +0100

    Add solution for puzzle 2025, 6b

--- advent/year_2025/puzzle_6/solve.py
+++ advent/year_2025/puzzle_6/solve.py
@@ -28,6 +28,36 @@ def solve_a(puzzle_input: list[str], example: bool) -> None:
     print(f"Solution is {solution}")
 
 
+def parse_digit(digit: str) -> int | None:
+    if digit == " ":
+        return None
+    else:
+        return int(digit)
+
+
 @register_solver(year="2025", key="6", variation="b")
 def solve_b(puzzle_input: list[str], example: bool) -> None:
-    print(f"Solution is TBD")
+    solution = 0
+
+    line_generators = [iter(line) for line in puzzle_input[:-1]]
+    operator_generator = PROBLEM_DEFINITION_PATTERN.finditer(puzzle_input[-1])
+
+    while operator_match := next(operator_generator, None):
+        operator = operator_match.group(0)
+        if operator == "+":
+            func = sum
+        elif operator == "*":
+            func = partial(prod, start=1)
+        else:
+            raise ValueError(f"Unsupported operator {operator}")
+
+        numbers: list[int] = []
+        next_vertical = [parse_digit(next(number_generator, "0")) for number_generator in line_generators]
+        while any(digit for digit in next_vertical if digit is not None):
+            numbers.append(int("".join(str(digit) for digit in next_vertical if digit is not None)))
+            next_vertical = [parse_digit(next(number_generator, "0")) for number_generator in line_generators]
+
+        print(f"{operator=}, {numbers=}")
+        solution += func(numbers)
+
+    print(f"Solution is {solution}")
