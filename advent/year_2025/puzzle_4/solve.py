5b3e6c632aed839454df644c84793ddc3dba7e05
Kharybdys <kharybdys@gmail.com>
2025-12-06 18:12:14 +0100

    Add solution for puzzle 2025, 4a

--- advent/year_2025/puzzle_4/solve.py
+++ advent/year_2025/puzzle_4/solve.py
@@ -1,9 +1,40 @@
+from collections.abc import Generator
+
 from advent.registry import register_solver
+from advent.utils.enums import PrintEnum
+from advent.utils.grid import Grid, Coords
+
+
+class TileStatus(PrintEnum):
+    PAPER = "PAPER", "@"
+    EMPTY = "EMPTY", "."
+
+
+TileGrid = Grid[TileStatus]
+
+
+def generate_eight_neighbours(from_coords: Coords) -> Generator[Coords, None, None]:
+    for diff_x in range(-1, 2):
+        for diff_y in range(-1, 2):
+            if diff_x != 0 or diff_y != 0:
+                yield Coords(x=from_coords.x + diff_x, y=from_coords.y + diff_y)
 
 
 @register_solver(year="2025", key="4", variation="a")
 def solve_a(puzzle_input: list[str], example: bool) -> None:
-    print(f"Solution is TBD")
+    print(puzzle_input)
+    grid: TileGrid = Grid.from_lines(puzzle_input, TileStatus.from_char)
+    solution = 0
+
+    for x, y in grid.all_coords_for({TileStatus.PAPER}):
+        paper_neighbours = 0
+        for neighbour in generate_eight_neighbours(Coords(x=x, y=y)):
+            if grid.value_at_or(neighbour.x, neighbour.y, default=TileStatus.EMPTY) == TileStatus.PAPER:
+                paper_neighbours += 1
+        if paper_neighbours < 4:
+            solution += 1
+
+    print(f"Solution is {solution}")
 
 
 @register_solver(year="2025", key="4", variation="b")
