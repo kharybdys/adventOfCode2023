8c875956ad625fd1b33e96906f5233ca9becffa2
Kharybdys <kharybdys@gmail.com>
2025-12-06 17:05:04 +0100

    Solve 2025, puzzle 1 b

--- advent/year_2025/puzzle_1/solve.py
+++ advent/year_2025/puzzle_1/solve.py
@@ -2,15 +2,29 @@ from advent.registry import register_solver
 
 
 class Dial:
-    def __init__(self):
+    def __init__(self, during: bool):
         self._position = 50
         self._count_zeroes = 0
+        self._during = during
 
     def move_dial(self, code: str):
-        self._position += self._code_to_diff(code)
+        diff = self._code_to_diff(code)
+
+        if self._during:
+            roundtrips = int(diff / 100)
+            self._count_zeroes += abs(roundtrips)
+            diff -= roundtrips * 100
+
+        original_position = self._position
+        self._position += diff
+        position_before_correction = self._position
         self._position = self._position % 100
+
         if self._position == 0:
             self._count_zeroes += 1
+        elif original_position != 0 and self._during:
+            if position_before_correction != self._position:
+                self._count_zeroes += 1
 
     def _code_to_diff(self, code: str) -> int:
         if code.startswith("L"):
@@ -26,7 +40,7 @@ class Dial:
 
 @register_solver(year="2025", key="1", variation="a")
 def solve_a(puzzle_input: list[str], example: bool) -> None:
-    dial = Dial()
+    dial = Dial(during=False)
     for line in puzzle_input:
         dial.move_dial(line)
 
@@ -35,4 +49,8 @@ def solve_a(puzzle_input: list[str], example: bool) -> None:
 
 @register_solver(year="2025", key="1", variation="b")
 def solve_b(puzzle_input: list[str], example: bool) -> None:
-    pass
+    dial = Dial(during=True)
+    for line in puzzle_input:
+        dial.move_dial(line)
+
+    print(f"Solution is {dial.count_zeroes=}")
