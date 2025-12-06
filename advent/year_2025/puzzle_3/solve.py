cfab16ce3f2fbd697fbf1b4e23ddf73f5e13796f
Kharybdys <kharybdys@gmail.com>
2025-12-06 17:33:21 +0100

    Add skeleton for puzzle 2025, 3

---
+++ advent/year_2025/puzzle_3/solve.py
@@ -0,0 +1,11 @@
+from advent.registry import register_solver
+
+
+@register_solver(year="2025", key="3", variation="a")
+def solve_a(puzzle_input: list[str], example: bool) -> None:
+    print(f"Solution is TBD")
+
+
+@register_solver(year="2025", key="3", variation="b")
+def solve_b(puzzle_input: list[str], example: bool) -> None:
+    print(f"Solution is TBD")
