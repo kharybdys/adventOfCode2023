73bdf487a1b4244e7f29bdcf34a12bb1a8db9bdd
Kharybdys <kharybdys@gmail.com>
2025-12-07 12:09:44 +0100

    Solution for 2025, 7b

--- advent/year_2025/puzzle_7/solve.py
+++ advent/year_2025/puzzle_7/solve.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 from collections.abc import Generator
 
 from advent.registry import register_solver
@@ -12,6 +13,11 @@ class Beam:
         start_x = self._process_start_line(line)
         self._tachyon_xs = {start_x}
         self.split_count = 0
+        self._timeline_count_per_x = {start_x: 1}
+
+    @property
+    def timelines_sum(self) -> int:
+        return sum(self._timeline_count_per_x.values())
 
     def _process_start_line(self, line: str) -> int:
         start_x = None
@@ -26,11 +32,10 @@ class Beam:
             raise ValueError(f"No S found in starting line")
         return start_x
 
-    def _new_xs_after_split(self, x: int, existing_beam_xs: set[int]) -> Generator[int, None, None]:
+    def _new_xs_after_split(self, x: int) -> Generator[int, None, None]:
         for new_x in {x-1, x+1}:
             if 0 <= new_x <= self._max_x:
-                if new_x not in existing_beam_xs:
-                    yield new_x
+                yield new_x
 
     def process_line(self, line: str):
         if not NON_STARTING_LINE_PATTERN.fullmatch(line):
@@ -39,16 +44,20 @@ class Beam:
         splitter_indices = {i for i, char in enumerate(line) if char == "^"}
         print(f"With {self.split_count} and beams at {self._tachyon_xs}")
         print(f"Processing splitters at {splitter_indices}")
+
         new_tachyon_xs = set()
+        new_timelines_per_x = defaultdict(int)
         for x in sorted(self._tachyon_xs):
             if x in splitter_indices:
-                new_xs = set(self._new_xs_after_split(x, new_tachyon_xs))
-                print(f"At splitter {x}, {new_xs=}")
                 self.split_count += 1
-                new_tachyon_xs.update(new_xs)
+                new_xs = set(self._new_xs_after_split(x))
             else:
-                new_tachyon_xs.add(x)
+                new_xs = {x}
+            new_tachyon_xs.update(new_xs)
+            for new_x in new_xs:
+                new_timelines_per_x[new_x] += self._timeline_count_per_x[x]
         self._tachyon_xs = new_tachyon_xs
+        self._timeline_count_per_x = new_timelines_per_x
 
 
 @register_solver(year="2025", key="7", variation="a")
@@ -63,5 +72,9 @@ def solve_a(puzzle_input: list[str], example: bool) -> None:
 
 @register_solver(year="2025", key="7", variation="b")
 def solve_b(puzzle_input: list[str], example: bool) -> None:
-    solution = 0
-    print(f"Solution is {solution}")
+    input_generator = iter(puzzle_input)
+    beam = Beam(next(input_generator))
+    for line in input_generator:
+        beam.process_line(line)
+
+    print(f"Solution is {beam.timelines_sum}")
