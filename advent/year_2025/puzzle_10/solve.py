82cfce09b3d0f3436f76cdd164ad1adff4adb930
Kharybdys <kharybdys@gmail.com>
2025-12-11 12:32:49 +0100

    Skeleton solution for 2025, 10.

---
+++ advent/year_2025/puzzle_10/solve.py
@@ -0,0 +1,13 @@
+from advent.registry import register_solver
+
+
+@register_solver(year="2025", key="10", variation="a")
+def solve_a(puzzle_input: list[str], example: bool) -> None:
+    solution = 0
+    print(f"Solution is {solution}")
+
+
+@register_solver(year="2025", key="10", variation="b")
+def solve_b(puzzle_input: list[str], example: bool) -> None:
+    solution = 0
+    print(f"Solution is {solution}")
